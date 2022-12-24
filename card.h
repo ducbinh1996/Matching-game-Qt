145084a087f15a1b4f5499e95cb9c21a6a0813b6
Binh Trieu <ducbinh1996@gmail.com>
2022-12-25 01:33:27 +0700

    first release version

--- card.h
+++ card.h
@@ -15,20 +15,29 @@
 class Card
 {
 public:
+    ~Card()
+    {
+        delete ui;
+    }
+
     void initialize(Ui::MainWindow *ui);
     bool get_card_state();
     void disable_cards(uint card1, uint card2);
     void change_card_availability(bool);
     bool is_matched_cards(uint card1, uint card2);
     void card_compare(uint card1, uint card2);
-    void card_handler(uint cardId, bool status);
+    bool card_handler(uint cardId, bool status);
     void add_random_images();
+    void set_started_status(bool);
 private:
+    bool game_started = false;
     Ui::MainWindow *ui;
     std::unordered_map<int, QCheckBox*> checkbox_map;
     int num_open_cards = 0;
     int pre_open_card = 0; //0 mean invalid
     std::array<QString, 16> appendStyleSheet;
+    int player_move = 0;
+    uint total_pair = 0;
 };
 
 #endif // CARD_H
